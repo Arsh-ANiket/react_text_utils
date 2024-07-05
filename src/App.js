24458dab246500ed87c69561eda0b59a35f56e21
Arsh <arshaniket@gmail.com>
2024-07-05 13:40:25 +0530

    React Router dom is used

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Text from "./components/Text";
 import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,  
+} from "react-router-dom";
+
+
 function App() {
   const [Mode, setMode] = useState('light');
   const [alert,setAlert]= useState(null);
@@ -31,15 +38,23 @@ function App() {
   }
   return (
     <>
+    <Router>
       <Navbar title="Text Utils" aboutText="ABOUT" mode={Mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <Text heading="Enter the text to be analyzed" mode={Mode}
-          showAlert={showAlert}
-        />
-      </div> 
-      <div className="container"><About/></div>
       
+      <Alert alert={alert}/>
+      
+      <div className="container my-3">
+        <Routes>
+          {/* Exact is used for exact matching, or else it always does partial matching, so 
+          it might not always give accurate Routes
+          eg:
+          /users/component1
+          /user/component2 */}
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/" element={<Text heading="Enter the text to be analyzed" mode={Mode} showAlert={showAlert} />} />
+        </Routes>
+      </div> 
+      </Router>
     </>
   );
 }
